4442113f390a3bc8e3185a6933ed3257b0e0f157
kwaku96 <kwakukusi30@outlook.com>
2020-03-25 11:17:29 +0300

    still fixing 1

--- src/settings.py
+++ src/settings.py
@@ -164,12 +164,12 @@ ACCOUNT_UNIQUE_EMAIL = True
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'account_login'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'gasmillasuarez@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get('APP_PASS') #'uksoiwcaeargewci'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'gasmillasuarez@gmail.com'
+# EMAIL_HOST_PASSWORD = os.environ.get('APP_PASS')
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
 
 django_heroku.settings(locals())
