b557e001672c20456c8b67e561454cae111c2384
kwaku96 <kwakukusi30@outlook.com>
2020-03-30 06:19:24 +0300

    ok all set

--- src/settings.py
+++ src/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -165,8 +165,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'gasmillasuarez@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get('APP_PASS')
+EMAIL_HOST_PASSWORD = 'uksoiwcaeargewci'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
